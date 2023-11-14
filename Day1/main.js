e64db335262fdd3c3ce30aac06718d11fd48a793
gurucharan <gurucharan.chouhan@skuad.io>
2023-11-14 22:21:32 +0530

    just check

--- Day1/main.js
+++ Day1/main.js
@@ -5,11 +5,7 @@ function getBestPair(arr){
    let n=arr.length;
  
     for(let i=0;i<n;i++)
-    for(let j=0;j<n;j++)
-        if(arr[i]==arr[j] && i<j)
-        {
-        bestPairCount++;
-        }
+   
     return bestPairCount; 
     
 }
